7179d4ec9e4f51024d072a98232901d7a12d2780
Ricardo <140122993+ricardovsa8@users.noreply.github.com>
2026-03-03 17:11:18 -0500

    Add files via upload

---
+++ Obtener  y actualizar estado de Firewall.ps1
@@ -0,0 +1,26 @@
+﻿#Obtener estado de Firewall
+Write-Output "Estados de perfiles"
+Get-NetFirewallProfile | Select-Object name, enabled
+Write-Output "+++++++++++++++++++++++"
+#Preguntar de cual perfil quiere cambiar su estado
+Write-Output "1-Domain"
+Write-Output "2-Private"
+Write-Output "3-Public"
+Write-Output "4-Salir"
+$valor_perfil = Read-Host "Seleccione el perfil que quiere actualizar su estado"
+switch ($valor_perfil){
+"1" {"Domain"}
+"2" {"Private"}
+"3" {"Public"}
+"4" {"Atras"}
+
+
+Default {"Ninguna opcion"}
+}
+
+
+
+
+
+#Get-NetFirewallRule | Select-Object name, enabled
+
